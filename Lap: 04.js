820b94d2cebc23ecb5e116cd65e8dde1628aae4d
Qusai Saqer <70547611+QusaiS@users.noreply.github.com>
2021-05-27 18:06:46 +0300

    Update Lap: 04.js

--- Lap: 04.js
+++ Lap: 04.js
@@ -1,1 +1,10 @@
-hello
+var userName=prompt('what is your name?  '); alert('Welcome ' + userName+' to our website'); 
+
+var important=prompt('did you creat website befor?  ');
+
+alert('Thats amazing Here you will learn how to draw your first website '); 
+
+var Wireframing=prompt('Are you interested to learn about Wireframing if yes ress 1')
+
+if (Wireframing == 1) { alert('Lets go '); }
+else { alert('Welcome to our website '); }
